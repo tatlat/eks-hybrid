7e5079be19731eb1c59f0b5842945ae4db6e0fac
Shizhao Liu <lshizhao@amazon.com>
2024-10-02 03:08:55 +0000

    Test nodeadm uninstall

--- test/integration/cases/install-containerd-source-none/run.sh
+++ test/integration/cases/install-containerd-source-none/run.sh
@@ -12,17 +12,35 @@ wait::dbus-ready
 # remove previously installed containerd to test installation via nodeadm
 dnf remove -y containerd
 
-nodeadm install 1.30  --credential-provider iam-ra --containerd-source none
+declare SUPPORTED_VERSIONS=(1.26 1.27 1.28 1.29 1.30)
 
-# /usr/bin/containerd not exists means nodeadm did not install containerd from any source
-assert::path-not-exist /usr/bin/containerd
-assert::path-exists /usr/sbin/iptables
-assert::path-exists /usr/bin/kubelet
-assert::path-exists /usr/local/bin/kubectl
-assert::path-exists /opt/cni/bin/
-assert::path-exists /etc/eks/image-credential-provider/ecr-credential-provider
-assert::path-exists /usr/local/bin/aws-iam-authenticator
+for VERSION in ${SUPPORTED_VERSIONS}
+do
+    nodeadm install $VERSION  --credential-provider iam-ra --containerd-source none
 
-assert::path-exists /usr/local/bin/aws_signing_helper
+    # /usr/bin/containerd not exists means nodeadm did not install containerd from any source
+    assert::path-not-exist /usr/bin/containerd
+    assert::path-exists /usr/sbin/iptables
+    assert::path-exists /usr/bin/kubelet
+    assert::path-exists /usr/local/bin/kubectl
+    VERSION_INFO=$(/usr/local/bin/kubectl version || true)
+    assert::is-substring "$VERSION_INFO" "v$VERSION"
+    assert::path-exists /opt/cni/bin/
+    assert::path-exists /etc/eks/image-credential-provider/ecr-credential-provider
+    assert::path-exists /usr/local/bin/aws-iam-authenticator
 
-assert::files-equal /opt/aws/nodeadm-tracker expected-nodeadm-tracker
+    assert::path-exists /usr/local/bin/aws_signing_helper
+
+    assert::files-equal /opt/aws/nodeadm-tracker expected-nodeadm-tracker
+
+    nodeadm uninstall
+
+    assert::path-not-exist /usr/sbin/iptables
+    assert::path-not-exist /usr/bin/kubelet
+    assert::path-not-exist /usr/local/bin/kubectl
+    assert::path-not-exist /opt/cni/bin/
+    assert::path-not-exist /etc/eks/image-credential-provider/ecr-credential-provider
+    assert::path-not-exist /usr/local/bin/aws-iam-authenticator
+    assert::path-not-exist /usr/local/bin/aws_signing_helper
+    assert::path-not-exist /opt/aws/nodeadm-tracker
+done
