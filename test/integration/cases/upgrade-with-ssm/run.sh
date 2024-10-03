519c2784a42cd26a50ebfc5bcacfeb2bdfbc9f71
Shizhao Liu <lshizhao@amazon.com>
2024-10-03 19:37:58 +0000

    Add integration test for nodeadm upgrade

---
+++ test/integration/cases/upgrade-with-ssm/run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source /helpers.sh
+
+mock::aws
+wait::dbus-ready
+
+declare INITIAL_VERSION=1.26
+declare TARGET_VERSION=1.30
+
+# remove previously installed containerd to test installation via nodeadm
+dnf remove -y containerd
+
+# Test nodeadm upgrade with ssm as credential provider
+# initial: version 1.26
+# target: version 1.30
+nodeadm install $INITIAL_VERSION --credential-provider ssm
+assert::path-exists /usr/bin/containerd
+assert::path-exists /usr/sbin/iptables
+assert::path-exists /usr/bin/kubelet
+assert::path-exists /usr/local/bin/kubectl
+VERSION_INFO=$(/usr/local/bin/kubectl version || true)
+assert::is-substring "$VERSION_INFO" "v$INITIAL_VERSION"
+assert::path-exists /opt/cni/bin/
+assert::path-exists /etc/eks/image-credential-provider/ecr-credential-provider
+assert::path-exists /usr/local/bin/aws-iam-authenticator
+assert::path-exists /opt/aws/ssm-setup-cli
+assert::files-equal /opt/aws/nodeadm-tracker expected-nodeadm-tracker
+
+nodeadm init --skip run,preprocess --config-source file://config.yaml
+
+nodeadm upgrade $TARGET_VERSION --skip run,preprocess,pod-validation,node-validation --config-source file://config.yaml
+assert::path-exists /usr/bin/containerd
+assert::path-exists /usr/sbin/iptables
+assert::path-exists /usr/local/bin/kubectl
+VERSION_INFO=$(/usr/local/bin/kubectl version || true)
+assert::is-substring "$VERSION_INFO" "v$TARGET_VERSION"
+assert::path-exists /opt/cni/bin/
+assert::path-exists /etc/eks/image-credential-provider/ecr-credential-provider
+assert::path-exists /usr/local/bin/aws-iam-authenticator
+assert::path-exists /opt/aws/ssm-setup-cli
+assert::files-equal /opt/aws/nodeadm-tracker expected-nodeadm-tracker
