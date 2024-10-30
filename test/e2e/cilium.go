5e921b2c85cda4b3a35c11e86987b2b2ae164aaf
Pankti Shah <shah.pankti2609@gmail.com>
2024-10-29 22:23:14 -0700

    Install Cilium on EKS hybrid cluster for CI

---
+++ test/e2e/cilium.go
@@ -0,0 +1,86 @@
+package e2e
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"fmt"
+	"html/template"
+	"strings"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+)
+
+//go:embed testdata/cilium-template.yaml
+var ciliumTemplate []byte
+
+type cilium struct {
+	K8s dynamic.Interface
+	// PodCIDR is the cluster level CIDR to be use for Pods. It needs to be big enough for
+	// Hybrid Nodes.
+	//
+	// Check the cilium-template file for the node pod cidr mask. The default is 24.
+	PodCIDR string
+
+	ciliumValuesFile string
+}
+
+func newCilium(k8s dynamic.Interface, podCIDR string) cilium {
+	return cilium{
+		K8s:              k8s,
+		PodCIDR:          podCIDR,
+		ciliumValuesFile: "testdata/cilium-values.yaml",
+	}
+}
+
+// deploy creates or updates the Cilium reosurces.
+func (c cilium) deploy(ctx context.Context) error {
+	tmpl, err := template.New("cilium").Parse(string(ciliumTemplate))
+	if err != nil {
+		return err
+	}
+	values := make(map[string]string)
+	values["PodCIDR"] = c.PodCIDR
+	installation := &bytes.Buffer{}
+	err = tmpl.Execute(installation, values)
+	if err != nil {
+		return err
+	}
+
+	objs, err := yamlToUnstructured(installation.Bytes())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Applying cilium installation")
+	for _, obj := range objs {
+		o := obj
+		groupVersion := o.GroupVersionKind()
+		resource := schema.GroupVersionResource{
+			Group:    groupVersion.Group,
+			Version:  groupVersion.Version,
+			Resource: strings.ToLower(groupVersion.Kind + "s"),
+		}
+		k8s := c.K8s.Resource(resource).Namespace(obj.GetNamespace())
+		if _, err := k8s.Get(ctx, obj.GetName(), metav1.GetOptions{}); apierrors.IsNotFound(err) {
+			fmt.Printf("Creating cilium object %s (%s)", o.GetName(), groupVersion)
+			_, err := k8s.Create(ctx, &obj, metav1.CreateOptions{})
+			if err != nil {
+				return fmt.Errorf("creating cilium object %s (%s): %w", o.GetName(), groupVersion, err)
+			}
+		} else if err != nil {
+			return fmt.Errorf("reading cilium object %s (%s): %w", o.GetName(), groupVersion, err)
+		} else {
+			fmt.Printf("Updating cilium object %s (%s)", o.GetName(), groupVersion)
+			_, err := k8s.Update(ctx, &obj, metav1.UpdateOptions{})
+			if err != nil {
+				return fmt.Errorf("updating cilium object %s (%s): %w", o.GetName(), groupVersion, err)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ test/e2e/unstructured.go
@@ -0,0 +1,53 @@
+package e2e
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	apiyaml "k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/yaml"
+)
+
+// yamlToUnstructured takes a YAML and converts it to a list of Unstructured objects.
+func yamlToUnstructured(rawyaml []byte) ([]unstructured.Unstructured, error) {
+	var ret []unstructured.Unstructured
+
+	reader := apiyaml.NewYAMLReader(bufio.NewReader(bytes.NewReader(rawyaml)))
+	count := 1
+	for {
+		// Read one YAML document at a time, until io.EOF is returned
+		b, err := reader.Read()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, fmt.Errorf("failed to read yaml: %w", err)
+		}
+		if len(b) == 0 {
+			break
+		}
+
+		var m map[string]interface{}
+		if err := yaml.Unmarshal(b, &m); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal the %d yaml document: %q: %w", count, string(b), err)
+		}
+
+		var u unstructured.Unstructured
+		u.SetUnstructuredContent(m)
+
+		// Ignore empty objects.
+		// Empty objects are generated if there are weird things in manifest files like e.g. two --- in a row without a yaml doc in the middle
+		if u.Object == nil {
+			continue
+		}
+
+		ret = append(ret, u)
+		count++
+	}
+
+	return ret, nil
+}
