eda354135d69e6f9dc3f4eb6249f62685f6f8f1c
Guillermo Gaston <gaslor@amazon.com>
2024-11-18 16:53:01 +0000

    Only create eks cluster if it doesn't exist

--- test/e2e/cluster.go
+++ test/e2e/cluster.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/eks"
 )
@@ -121,10 +122,105 @@ type RemotePodNetwork struct {
 	CIDRs []*string `locationName:"cidrs" type:"list"`
 }
 
-func CreateCluster(ctx context.Context, client *eks.EKS, input *CreateClusterInput, opts ...request.Option) (*eks.CreateClusterOutput, error) {
+type CreateClusterOutput struct {
+	_ struct{} `type:"structure"`
+
+	// The full description of your specified cluster.
+	Cluster *Cluster `locationName:"cluster" type:"structure"`
+}
+
+// An object representing an Amazon EKS cluster.
+type Cluster struct {
+	_ struct{} `type:"structure"`
+
+	// The access configuration for the cluster.
+	AccessConfig *eks.AccessConfigResponse `locationName:"accessConfig" type:"structure"`
+
+	// The Amazon Resource Name (ARN) of the cluster.
+	Arn *string `locationName:"arn" type:"string"`
+
+	// The certificate-authority-data for your cluster.
+	CertificateAuthority *eks.Certificate `locationName:"certificateAuthority" type:"structure"`
+
+	// A unique, case-sensitive identifier that you provide to ensure the idempotency
+	// of the request.
+	ClientRequestToken *string `locationName:"clientRequestToken" type:"string"`
+
+	// The configuration used to connect to a cluster for registration.
+	ConnectorConfig *eks.ConnectorConfigResponse `locationName:"connectorConfig" type:"structure"`
+
+	// The Unix epoch timestamp at object creation.
+	CreatedAt *time.Time `locationName:"createdAt" type:"timestamp"`
+
+	// The encryption configuration for the cluster.
+	EncryptionConfig []*eks.EncryptionConfig `locationName:"encryptionConfig" type:"list"`
+
+	// The endpoint for your Kubernetes API server.
+	Endpoint *string `locationName:"endpoint" type:"string"`
+
+	// An object representing the health of your Amazon EKS cluster.
+	Health *eks.ClusterHealth `locationName:"health" type:"structure"`
+
+	// The ID of your local Amazon EKS cluster on an Amazon Web Services Outpost.
+	// This property isn't available for an Amazon EKS cluster on the Amazon Web
+	// Services cloud.
+	Id *string `locationName:"id" type:"string"`
+
+	// The identity provider information for the cluster.
+	Identity *eks.Identity `locationName:"identity" type:"structure"`
+
+	// The Kubernetes network configuration for the cluster.
+	KubernetesNetworkConfig *eks.KubernetesNetworkConfigResponse `locationName:"kubernetesNetworkConfig" type:"structure"`
+
+	// The logging configuration for your cluster.
+	Logging *eks.Logging `locationName:"logging" type:"structure"`
+
+	// The name of your cluster.
+	Name *string `locationName:"name" type:"string"`
+
+	// An object representing the configuration of your local Amazon EKS cluster
+	// on an Amazon Web Services Outpost. This object isn't available for clusters
+	// on the Amazon Web Services cloud.
+	OutpostConfig *eks.OutpostConfigResponse `locationName:"outpostConfig" type:"structure"`
+
+	// The platform version of your Amazon EKS cluster. For more information about
+	// clusters deployed on the Amazon Web Services Cloud, see Platform versions
+	// (https://docs.aws.amazon.com/eks/latest/userguide/platform-versions.html)
+	// in the Amazon EKS User Guide . For more information about local clusters
+	// deployed on an Outpost, see Amazon EKS local cluster platform versions (https://docs.aws.amazon.com/eks/latest/userguide/eks-outposts-platform-versions.html)
+	// in the Amazon EKS User Guide .
+	PlatformVersion *string `locationName:"platformVersion" type:"string"`
+
+	// The VPC configuration used by the cluster control plane. Amazon EKS VPC resources
+	// have specific requirements to work properly with Kubernetes. For more information,
+	// see Cluster VPC considerations (https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html)
+	// and Cluster security group considerations (https://docs.aws.amazon.com/eks/latest/userguide/sec-group-reqs.html)
+	// in the Amazon EKS User Guide.
+	ResourcesVpcConfig *eks.VpcConfigResponse `locationName:"resourcesVpcConfig" type:"structure"`
+
+	// The Amazon Resource Name (ARN) of the IAM role that provides permissions
+	// for the Kubernetes control plane to make calls to Amazon Web Services API
+	// operations on your behalf.
+	RoleArn *string `locationName:"roleArn" type:"string"`
+
+	// The current status of the cluster.
+	Status *string `locationName:"status" type:"string" enum:"ClusterStatus"`
+
+	// Metadata that assists with categorization and organization. Each tag consists
+	// of a key and an optional value. You define both. Tags don't propagate to
+	// any other cluster or Amazon Web Services resources.
+	Tags map[string]*string `locationName:"tags" min:"1" type:"map"`
+
+	// The Kubernetes server version for the cluster.
+	Version *string `locationName:"version" type:"string"`
+
+	RemoteNetworkConfig *RemoteNetworkConfig `locationName:"remoteNetworkConfig" type:"structure"`
+}
+
+func CreateCluster(ctx context.Context, client *eks.EKS, input *CreateClusterInput, opts ...request.Option) (*CreateClusterOutput, error) {
 	req, _ := client.CreateClusterRequest(&eks.CreateClusterInput{})
 	req.Params = input
-	out := &eks.CreateClusterOutput{}
+	out := &CreateClusterOutput{}
 	req.Data = out
 	req.SetContext(ctx)
 	req.ApplyOptions(opts...)
@@ -161,11 +257,14 @@ func (t *TestRunner) createEKSCluster(ctx context.Context, clusterName, kubernet
 		},
 	}
 	clusterOutput, err := CreateCluster(ctx, svc, eksCluster)
-	if err != nil {
+	if err != nil && !isErrCode(err, eks.ErrCodeResourceInUseException) {
 		return fmt.Errorf("creating EKS hybrid cluster: %w", err)
 	}
 
-	fmt.Printf("Successfully started EKS hybrid cluster: %s\nOutput: %s", clusterName, clusterOutput)
+	if clusterOutput.Cluster != nil {
+		fmt.Printf("Successfully started EKS hybrid cluster: %s\nOutput: %s\n", clusterName, awsutil.Prettify(clusterOutput))
+	}
+
 	return nil
 }
 
