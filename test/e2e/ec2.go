39168dbd517ff310bddca8cf29da647d3ac7637a
Jackson West <jgw@amazon.com>
2024-11-19 11:56:39 -0600

    waits for ssm node to be deregistered

--- test/e2e/ec2.go
+++ test/e2e/ec2.go
@@ -199,3 +199,50 @@ func rebootEC2Instance(ctx context.Context, client *ec2.Client, instanceID strin
 	}
 	return nil
 }
+
+func waitForManagedInstanceUnregistered(ctx context.Context, ssmClient *ssm.SSM, instanceId string) error {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Minute)
+	defer cancel()
+
+	statusCh := make(chan struct{})
+	errCh := make(chan error)
+	consecutiveErrors := 0
+
+	go func() {
+		defer close(statusCh)
+		defer close(errCh)
+		for {
+			output, err := ssmClient.DescribeInstanceInformationWithContext(ctx, &ssm.DescribeInstanceInformationInput{
+				Filters: []*ssm.InstanceInformationStringFilter{
+					{
+						Key:    aws.String("InstanceIds"),
+						Values: []*string{aws.String(instanceId)},
+					},
+				},
+			})
+			if err != nil {
+				consecutiveErrors += 1
+				if consecutiveErrors > 3 || ctx.Err() != nil {
+					errCh <- fmt.Errorf("failed to describe instance information %s: %v", instanceId, err)
+					return
+				}
+			} else if len(output.InstanceInformationList) == 0 {
+				statusCh <- struct{}{}
+				return
+			} else {
+				consecutiveErrors = 0
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	select {
+	case <-statusCh:
+		return nil
+	case err := <-errCh:
+		return err
+	case <-ctx.Done():
+		return fmt.Errorf("timed out waiting for instance to unregister: %s", instanceId)
+	}
+}
