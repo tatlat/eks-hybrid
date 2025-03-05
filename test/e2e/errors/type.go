6c407d2c44e9c985237f378c0648333d21f0f58f
Guillermo Gaston <gaslor@amazon.com>
2025-03-05 17:35:53 +0000

    Fix bug on errors.IsType helper
    If we take an empty interface, the errors.As receives as such instead of
    as the original type passed to our helper. Since all errors can be
    assigned to an empty interface, our helper always returns true. By
    making the function take a generic type we both ensure that errors.As
    receives the right type as well as enforcing that our helper only takes
    types that implement error, which is required by errors.As.

--- test/e2e/errors/type.go
+++ test/e2e/errors/type.go
@@ -3,6 +3,6 @@ package errors
 import "errors"
 
 // IsType returns true if any errors in the chain are of the specified type.
-func IsType(err error, targetType interface{}) bool {
+func IsType[T error](err error, targetType T) bool {
 	return errors.As(err, &targetType)
 }
