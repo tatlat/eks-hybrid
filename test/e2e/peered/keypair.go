415503fc5708e712b9db4e21436544e8feb0b485
junpengdev <junpengdev@gmail.com>
2025-08-19 12:40:16 -0700

    Upgrade aws-sdk-go/aws to aws-sdk-go-v2/aws

--- test/e2e/peered/keypair.go
+++ test/e2e/peered/keypair.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/aws/eks-hybrid/test/e2e/constants"
 )
