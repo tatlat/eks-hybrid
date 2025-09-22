111e102e381a54b9cd8a9a1a9bc8959879792fb3
ppadyala1599 <ppadyala@amazon.com>
2025-09-22 11:45:05 -0700

    S3 upload scripts by removing explicit public-read ACL flags (#745)

--- hack/release-nodeadm.sh
+++ hack/release-nodeadm.sh
@@ -6,20 +6,14 @@ set -o pipefail
 # Required arguments
 PROD_BUCKET=$1
 VERSION=$2
-PUBLIC_READ_ACL="${3:-true}"
-
-PUBLIC_READ_ACL_ARG=""  
-if [ "$PUBLIC_READ_ACL" = "true" ]; then
-  PUBLIC_READ_ACL_ARG="--acl public-read"
-fi
 
 echo "Starting nodeadm release process..."
 
 # Upload to production
 echo "Uploading artifacts to production..."
-aws s3 sync --no-progress --exclude "*nodeadm.gz" latest/ s3://${PROD_BUCKET}/releases/${VERSION}/ ${PUBLIC_READ_ACL_ARG}
+aws s3 sync --no-progress --exclude "*nodeadm.gz" latest/ s3://${PROD_BUCKET}/releases/${VERSION}/
 # uploading nodeadm.gz files separately to ensure the content-encoding/disposition is applied
-aws s3 sync --no-progress --include "*nodeadm.gz" --content-encoding gzip --content-disposition "attachment; filename=\"nodeadm\"" latest/ s3://${PROD_BUCKET}/releases/${VERSION}/ ${PUBLIC_READ_ACL_ARG}
+aws s3 sync --no-progress --include "*nodeadm.gz" --content-encoding gzip --content-disposition "attachment; filename=\"nodeadm\"" latest/ s3://${PROD_BUCKET}/releases/${VERSION}/
 
 # Update latest symlinks
 echo "Updating latest symlinks for nodeadm..."
--- hack/upload-dev-release-artifacts.sh
+++ hack/upload-dev-release-artifacts.sh
@@ -4,12 +4,6 @@ set -o nounset
 set -o pipefail
 
 ARTIFACTS_BUCKET="$1"
-PUBLIC_READ_ACL="${2:-true}"
-
-PUBLIC_READ_ACL_ARG=""  
-if [ "$PUBLIC_READ_ACL" = "true" ]; then
-  PUBLIC_READ_ACL_ARG="--acl public-read"
-fi
 
 # only uploading nodeadm, ATTRIBUTION.txt, and GIT_VERSION, skipping ginkgo/e2e-test/nodeadm.test
 mkdir -p _bin/latest/bin/linux/{amd64,arm64}
@@ -18,5 +12,5 @@ cp _bin/amd64/nodeadm{,.gz,.sha256,.sha512,.gz.sha256,.gz.sha512} _bin/latest/bi
 cp _bin/arm64/nodeadm{,.gz,.sha256,.sha512,.gz.sha256,.gz.sha512} _bin/latest/bin/linux/arm64/
 
 # uploading nodeadm.gz files separately to ensure the content-encoding/disposition is applied
-aws s3 sync --no-progress --exclude "*nodeadm.gz" _bin/latest/ s3://${ARTIFACTS_BUCKET}/latest/ ${PUBLIC_READ_ACL_ARG}
-aws s3 sync --no-progress --include "*nodeadm.gz" --content-encoding gzip --content-disposition "attachment; filename=\"nodeadm\"" _bin/latest/ s3://${ARTIFACTS_BUCKET}/latest/ ${PUBLIC_READ_ACL_ARG}
+aws s3 sync --no-progress --exclude "*nodeadm.gz" _bin/latest/ s3://${ARTIFACTS_BUCKET}/latest/
+aws s3 sync --no-progress --include "*nodeadm.gz" --content-encoding gzip --content-disposition "attachment; filename=\"nodeadm\"" _bin/latest/ s3://${ARTIFACTS_BUCKET}/latest/
