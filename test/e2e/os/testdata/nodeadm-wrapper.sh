3071868f4b6d13f898657c02a8c0a907030895ee
Tanvir Tatla <tatlat@amazon.com>
2026-06-19 11:39:29 -0700

    fix nodeadm wrapper to only pass manifest override to commands that support the flag

--- test/e2e/os/testdata/nodeadm-wrapper.sh
+++ test/e2e/os/testdata/nodeadm-wrapper.sh
@@ -4,8 +4,11 @@
 NODEADM_ARGS=("$@")
 
 {{- if .ManifestURL }}
-# Add --manifest-override flag if ManifestURL is provided
-NODEADM_ARGS=("--manifest-override" "{{ .ManifestURL }}" "${NODEADM_ARGS[@]}")
+# Add --manifest-override flag only for commands that support it
+COMMAND="${1:-}"
+if [[ "$COMMAND" == "init" || "$COMMAND" == "upgrade" || "$COMMAND" == "install" ]]; then
+  NODEADM_ARGS=("--manifest-override" "{{ .ManifestURL }}" "${NODEADM_ARGS[@]}")
+fi
 {{- end }}
 
 AWS_ENDPOINT_URL_EKS={{ .EKSEndpoint }} /usr/local/bin/nodeadm "${NODEADM_ARGS[@]}"
